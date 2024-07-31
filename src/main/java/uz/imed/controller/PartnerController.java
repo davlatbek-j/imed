5c6cf30c668eb0a0deb3a906d1fdb7f33f208e62
nodirjon2901 <nodirjontojiahmedov2901@gmail.com>
2024-07-31 17:35:05 +0500

    Commit by Nodir. Add partner header classes

--- src/main/java/uz/imed/controller/PartnerController.java
+++ src/main/java/uz/imed/controller/PartnerController.java
@@ -5,8 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import uz.imed.entity.Partner;
+import uz.imed.entity.PartnerHeader;
 import uz.imed.payload.ApiResponse;
 import uz.imed.payload.PartnerDTO;
+import uz.imed.payload.PartnerHeaderDTO;
+import uz.imed.service.PartnerHeaderService;
 import uz.imed.service.PartnerService;
 
 import java.util.List;
@@ -17,6 +20,8 @@ import java.util.List;
 public class PartnerController {
 
     private final PartnerService partnerService;
+
+    private final PartnerHeaderService partnerHeaderService;
 
     @PostMapping("/create")
     public ResponseEntity<ApiResponse<Partner>> create(
@@ -51,7 +56,7 @@ public class PartnerController {
     @PutMapping("/update")
     public ResponseEntity<ApiResponse<Partner>> update(
             @RequestBody Partner partner
-    ){
+    ) {
         return partnerService.update(partner);
     }
 
@@ -60,6 +65,37 @@ public class PartnerController {
             @PathVariable Long id
     ) {
         return partnerService.deleteById(id);
+    }
+
+    @PostMapping("/header/create")
+    public ResponseEntity<ApiResponse<PartnerHeader>> create(
+            @RequestBody PartnerHeader header
+    ) {
+        return partnerHeaderService.create(header);
+    }
+
+    @GetMapping("/header/get")
+    public ResponseEntity<ApiResponse<PartnerHeaderDTO>> find(
+            @RequestHeader(value = "Accept-Language") String lang
+    ) {
+        return partnerHeaderService.find(lang);
+    }
+
+    @GetMapping("/header/get-full-data")
+    public ResponseEntity<ApiResponse<PartnerHeader>> findFullData() {
+        return partnerHeaderService.findFullData();
+    }
+
+    @PutMapping("/header/update")
+    public ResponseEntity<ApiResponse<PartnerHeader>> update(
+            @RequestBody PartnerHeader header
+    ) {
+        return partnerHeaderService.update(header);
+    }
+
+    @DeleteMapping("/header/delete")
+    public ResponseEntity<ApiResponse<?>> delete() {
+        return partnerHeaderService.delete();
     }
 
 }
