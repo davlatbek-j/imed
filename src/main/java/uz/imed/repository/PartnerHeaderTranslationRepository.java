1074069dc5a783272ebe9307cb418c2ddd320d51
admin <theferganes@gmail.com>
2024-08-04 22:42:09 +0500

    Partner header approach-1

--- src/main/java/uz/imed/repository/PartnerHeaderTranslationRepository.java
+++
@@ -1,9 +0,0 @@
-package uz.imed.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import uz.imed.entity.PartnerHeaderTranslation;
-
-@Repository
-public interface PartnerHeaderTranslationRepository extends JpaRepository<PartnerHeaderTranslation, Long> {
-}
--- src/main/java/uz/imed/service/PartnerService.java
+++ src/main/java/uz/imed/service/PartnerService.java
@@ -29,12 +29,10 @@ public class PartnerService {
 
     private final PartnerRepository partnerRepository;
 
-
     private final PhotoService photoService;
 
     private final ObjectMapper objectMapper;
-    private final RestClient.Builder restClientBuilder;
-
+    
     public ResponseEntity<ApiResponse<Partner>> create(String json, MultipartFile logo) {
         ApiResponse<Partner> response = new ApiResponse<>();
         try {
@@ -53,81 +51,6 @@ public class PartnerService {
         }
     }
 
-    /*  public ResponseEntity<ApiResponse<PartnerDTO>> findBySlug(String slug, String lang) {
-          ApiResponse<PartnerDTO> response = new ApiResponse<>();
-          Partner partner = partnerRepository.findBySlug(slug).orElseThrow(() -> new NotFoundException("Partner is not found by slug: " + slug));
-          response.setData(new PartnerDTO(partner, lang));
-          return ResponseEntity.ok(response);
-      }
-
-      public ResponseEntity<ApiResponse<Partner>> findFullDataById(Long id) {
-          ApiResponse<Partner> response = new ApiResponse<>();
-          Partner partner = partnerRepository.findById(id).orElseThrow(() -> new NotFoundException("Partner is not found by id: " + id));
-          response.setData(partner);
-          return ResponseEntity.ok(response);
-      }
-
-      public ResponseEntity<ApiResponse<List<PartnerDTO>>> findAll(String lang) {
-          ApiResponse<List<PartnerDTO>> response = new ApiResponse<>();
-          response.setData(new ArrayList<>());
-          List<Partner> all = partnerRepository.findAll();
-          all.forEach(partner -> response.getData().add(new PartnerDTO(partner, lang)));
-          response.setMessage("Found " + all.size() + " partner(s)");
-          return ResponseEntity.ok(response);
-      }
-
-      public ResponseEntity<ApiResponse<Partner>> update(Partner newPartner) {
-          ApiResponse<Partner> response = new ApiResponse<>();
-          Partner existPartner = partnerRepository.findById(newPartner.getId()).orElseThrow(() -> new NotFoundException("Partner is not found by id: " + newPartner.getId()));
-
-          if (newPartner.getWebsite() != null) {
-              existPartner.setWebsite(newPartner.getWebsite());
-          }
-          if (newPartner.getActive() != null) {
-              existPartner.setActive(newPartner.getActive());
-          }
-          if (newPartner.getTranslations() != null) {
-              for (PartnerTranslation newTranslation : newPartner.getTranslations()) {
-                  PartnerTranslation existTranslation = existPartner.getTranslations()
-                          .stream()
-                          .filter(partnerTranslation -> partnerTranslation.getLanguage().equals(newTranslation.getLanguage()))
-                          .findFirst()
-                          .orElseThrow(null);
-                  if (existTranslation != null) {
-                      if (newTranslation.getName() != null) {
-                          if (newTranslation.getLanguage().equals("en")) {
-                              String slug = existPartner.getId() + "-" + SlugUtil.makeSlug(newTranslation.getName());
-                              existPartner.setSlug(slug);
-                          }
-                          existTranslation.setName(newTranslation.getName());
-                      }
-                      if (newTranslation.getNote() != null) {
-                          existTranslation.setNote(newTranslation.getNote());
-                      }
-                      if (newTranslation.getAbout() != null) {
-                          existTranslation.setAbout(newTranslation.getAbout());
-                      }
-                      existTranslation.setPartner(existPartner);
-                  }
-              }
-          }
-
-          Partner save = partnerRepository.save(existPartner);
-          response.setData(save);
-          return ResponseEntity.ok(response);
-      }
-
-      public ResponseEntity<ApiResponse<?>> deleteById(Long id) {
-          ApiResponse<?> response = new ApiResponse<>();
-          if (!partnerRepository.existsById(id)) {
-              throw new NotFoundException("Partner is not found by id: " + id);
-          }
-          partnerRepository.deleteById(id);
-          response.setMessage("Successfully deleted!");
-          return ResponseEntity.ok(response);
-      }
-  */
-    //TODO -----------------------
     public ResponseEntity<ApiResponse<?>> get(String slug, String lang) {
         Partner partner = partnerRepository.findBySlug(slug).orElseThrow(() -> new NotFoundException("Partner is not found by slug: " + slug));
         if (lang == null) {
