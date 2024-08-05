346c80e2d88b6530b7ce3234390589187b60c2c6
nodirjon2901 <nodirjontojiahmedov2901@gmail.com>
2024-08-05 10:30:43 +0500

    Commit by Nodir. add certificate classes

--- src/main/java/uz/imed/repository/ClientTranslationRepository.java
+++
@@ -1,9 +0,0 @@
-package uz.imed.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import uz.imed.entity.ClientTranslation;
-
-@Repository
-public interface ClientTranslationRepository extends JpaRepository<ClientTranslation, Long> {
-}
